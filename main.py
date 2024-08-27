b4cb3dfe45f34084a507aa0a3dd5fe34e146c3e9
Mike <30466141+Mike-kwaku@users.noreply.github.com>
2024-08-27 06:43:34 -0700

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def magic_link(email):
 
 
 
-col1 = st.columns
+col1 = st.columns(1)
 with col1:
         with st.expander('Generate Magic Link'):
             email = st.text_input('Email', key='enter email address')
